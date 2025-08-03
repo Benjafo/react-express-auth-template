2788bc37873cb6f590b5b78b508795b6dfcaa690
Benjafo <benjafo@gmail.com>
2025-08-03 18:11:20 -0400

    checkpoint: modify server/eslint.config.mjs - 18:11

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -17,7 +17,10 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }]
     }
   },
 )
